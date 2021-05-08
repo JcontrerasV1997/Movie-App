cf661da237b747decc780fba9d53884434785063
Juan Manuel <jmcontrerasvega@gmail.com>
2021-05-07 19:45:00 -0500

    update 3

--- docs/main.5eaaa729e0867492d60d.js
+++ docs/main.5eaaa729e0867492d60d.js
@@ -484,16 +484,123 @@ __webpack_require__.r(__webpack_exports__);
 var componentes = __webpack_require__(0);
 
 // CONCATENATED MODULE: ./src/js/eventos.js
-var Evento=function(){var a=document.getElementById("form"),b=document.getElementById("search");a.addEventListener("submit",function(a){a.preventDefault();var c=b.value;c&&""!==c?(consumoApi(SEARCH_URL+c),console.log(SEARCH_URL),b.value=""):window.location.reload()})};
+
+var eventos_Evento = function Evento() {
+  var form = document.getElementById("form");
+  var search = document.getElementById("search");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var searhTerm = search.value;
+
+    if (searhTerm && searhTerm !== "") {
+      consumoApi(SEARCH_URL + searhTerm);
+      console.log(SEARCH_URL);
+      search.value = "";
+    } else {
+      window.location.reload();
+    }
+  });
+};
 // CONCATENATED MODULE: ./src/js/http-provider.js
-function asyncGeneratorStep(a,b,c,d,e,f,g){try{var h=a[f](g),i=h.value}catch(a){return void c(a)}h.done?b(i):Promise.resolve(i).then(d,e)}function _asyncToGenerator(a){return function(){var b=this,c=arguments;return new Promise(function(d,e){function f(a){asyncGeneratorStep(h,d,e,f,g,"next",a)}function g(a){asyncGeneratorStep(h,d,e,f,g,"throw",a)}var h=a.apply(b,c);f(void 0)})}}var API_URL="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";var IMG_PATH="https://image.tmdb.org/t/p/w500";var SEARCH_URL="https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=\"";var consumoApi=/*#__PURE__*/function(){var a=_asyncToGenerator(/*#__PURE__*/regeneratorRuntime.mark(function a(){var b,c;return regeneratorRuntime.wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.prev=0,a.next=3,fetch(API_URL);case 3:if(b=a.sent,b.ok){a.next=6;break}return a.abrupt("return",alert("no se pudo realizar la peticion"));case 6:return a.next=8,b.json();case 8:return c=a.sent,mostrarPelicula(c.results),a.abrupt("return",c);case 13:throw a.prev=13,a.t0=a["catch"](0),a.t0;case 16:case"end":return a.stop();}},a,null,[[0,13]])}));return function(){return a.apply(this,arguments)}}();var mostrarPelicula=function(a){a.forEach(function(a){var b=a.title,c=a.poster_path,d=a.vote_average,e=a.overview,f=document.createElement("div");f.classList.add("movie"),f.innerHTML="\n    <img src=\"".concat(IMG_PATH+a.poster_path,"\" alt=\"").concat(a.title,"\" />\n    <div class=\"movie-info\">\n      <h3>").concat(a.title,"</h3>\n      <span class=\"").concat(puntuacionPelicula(a.vote_average),"\">").concat(a.vote_average,"</span>\n    </div>\n    <div class=\"overview\">\n      <h3>Overview</h3>\n     ").concat(a.overview,"\n    </div>"),main.appendChild(f)})};
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+
+
+var API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
+var IMG_PATH = "https://image.tmdb.org/t/p/w500";
+var SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+var consumoApi = /*#__PURE__*/function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    var resp, consumo;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.prev = 0;
+            _context.next = 3;
+            return fetch(API_URL);
+
+          case 3:
+            resp = _context.sent;
+
+            if (resp.ok) {
+              _context.next = 6;
+              break;
+            }
+
+            return _context.abrupt("return", alert("no se pudo realizar la peticion"));
+
+          case 6:
+            _context.next = 8;
+            return resp.json();
+
+          case 8:
+            consumo = _context.sent;
+            http_provider_mostrarPelicula(consumo.results);
+            return _context.abrupt("return", consumo);
+
+          case 13:
+            _context.prev = 13;
+            _context.t0 = _context["catch"](0);
+            throw _context.t0;
+
+          case 16:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, null, [[0, 13]]);
+  }));
+
+  return function consumoApi() {
+    return _ref.apply(this, arguments);
+  };
+}();
+var http_provider_mostrarPelicula = function mostrarPelicula(peliculas) {
+  peliculas.forEach(function (peliculas) {
+    var title = peliculas.title,
+        poster_path = peliculas.poster_path,
+        vote_average = peliculas.vote_average,
+        overview = peliculas.overview;
+    var listaPeli = document.createElement("div");
+    listaPeli.classList.add("movie");
+    listaPeli.innerHTML = "\n    <img src=\"".concat(IMG_PATH + peliculas.poster_path, "\" alt=\"").concat(peliculas.title, "\" />\n    <div class=\"movie-info\">\n      <h3>").concat(peliculas.title, "</h3>\n      <span class=\"").concat(puntuacionPelicula(peliculas.vote_average), "\">").concat(peliculas.vote_average, "</span>\n    </div>\n    <div class=\"overview\">\n      <h3>Overview</h3>\n     ").concat(peliculas.overview, "\n    </div>");
+    main.appendChild(listaPeli);
+  });
+};
 // CONCATENATED MODULE: ./src/js/componentes.js
-var header=document.querySelector("header");var main=document.querySelector("main");var Search=function(){header.innerHTML="\n  <form id=\"form\">\n      <input type=\"text\" id=\"search\" class=\"search\" placeholder=\"Search\">\n  </form>"};var Main=function(){consumoApi()};var puntuacionPelicula=function(a){if(8<=a)return"green";return 5<=a?"orange":void 0};
+
+
+var header = document.querySelector("header");
+var main = document.querySelector("main");
+var Search = function Search() {
+  var html = "\n  <form id=\"form\">\n      <input type=\"text\" id=\"search\" class=\"search\" placeholder=\"Search\">\n  </form>";
+  header.innerHTML = html;
+};
+var componentes_Main = function Main() {
+  consumoApi();
+};
+var puntuacionPelicula = function puntuacionPelicula(voto) {
+  if (voto >= 8) {
+    return "green";
+  } else if (voto >= 5) {
+    return "orange";
+  } else {
+    "red";
+  }
+};
 // EXTERNAL MODULE: ./src/styles.css
 var styles = __webpack_require__(4);
 
 // CONCATENATED MODULE: ./src/index.js
-Search(),Evento(),Main();
+
+
+
+Search();
+eventos_Evento();
+componentes_Main();
 
 /***/ })
 /******/ ]);
